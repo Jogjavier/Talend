4bf6940d3bde96abf8eed105d7abac301b4dce5b
jogjavier <jog02ortiz@gmail.com>
2024-09-25 13:48:45 -0600

    first commit

---
+++ Documents/Trsf_Otoño-2024/Ejercicios-Oto-o_2024/sesion2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Por favor introduzca su nombre");
+
+int numeroPersonas = 12;
+
+string nombrePersonas = Console.ReadLine();
+
+Console.WriteLine("En este salon hay {0} personas y una se llama {1}", numeroPersonas, nombrePersonas);
+
+Console.ReadKey();
